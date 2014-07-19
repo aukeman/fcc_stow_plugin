f0f152d6bf763cb4c816d8a45d2b4b864b3ee4ee
Jacob Aukeman <jacob.aukeman@gmail.com>
2014-07-19 00:02:21 -0400

    working on file uploads

--- templates/audio_file_meta_box.php
+++ templates/audio_file_meta_box.php
@@ -1,23 +1,26 @@
-<div>
-      <input style="width:100%"
-	     type="text" 
-    	     id="fcc-stow-sermon-audio-file" 
-	     name="fcc-stow-sermon-audio-file"
-	     value="<?php echo @get_post_meta($post->ID, 'fcc-stow-sermon-audio-file', true); ?>" />
-      <input type="button"
-	     id="fcc-stow-sermon-audio-file-button" 
-	     name="fcc-stow-sermon-audio-file-button" 
-	     value="Check Link" />
+<div id="fcc-stow-sermon-selected-audio-file-container">
+<?php 
+$input_tag_html="Upload Sermon Audio File
+      <input style=\"width:100%\"
+	     type=\"file\" 
+    	     id=\"fcc-stow-sermon-audio-file\" 
+	     name=\"fcc-stow-sermon-audio-file\"
+	     value=\"\" />";
+$audio_file=get_post_meta($post->ID, 'fcc-stow-sermon-audio-file', true);
+if ( isset($audio_file) ) : ?>
+  Sermon Audio File
+  <div><?php echo basename($audio_file["file"]) ?></div>
+  <button id="fcc-stow-sermon-pick-new-file-button">Pick a new file</button>
+<?php else : echo $input_tag_html ?>
+<?php endif ?>
 </div>
 <script type="text/javascript">
-
 jQuery(document).ready(function($){
 
-    $('#fcc-stow-sermon-audio-file-button').click(function(){
-    	var url = $("#fcc-stow-sermon-audio-file").val();
-    	window.open( url );
+    $('#fcc-stow-sermon-pick-new-file-button').click(function(){
+    
+       window.open( url );
       });
 
 });
-
 </script>
