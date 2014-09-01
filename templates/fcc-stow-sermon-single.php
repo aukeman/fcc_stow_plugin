d8db77e77acf0c47b7f5f4ab58d8eb5edfb5945b
Jacob Aukeman <jacob.aukeman@gmail.com>
2014-08-31 21:47:50 -0400

    apply class names to metadata containers

--- templates/fcc-stow-sermon-single.php
+++ templates/fcc-stow-sermon-single.php
@@ -1,4 +1,4 @@
-<div><?php echo get_the_date() ?></div>
-<div><?php fcc_stow_sermon_the_speaker() ?></div>
-<div><?php fcc_stow_sermon_the_scripture_passages() ?></div>
-<div><?php fcc_stow_sermon_the_audio_file_link() ?></div>
+<div class="fcc-stow-sermon-date-container"><?php echo get_the_date() ?></div>
+<div class="fcc-stow-sermon-speaker-container"><?php fcc_stow_sermon_the_speaker() ?></div>
+<div class="fcc-stow-sermon-scripture-container"><?php fcc_stow_sermon_the_scripture_passages() ?></div>
+<div class="fcc-stow-sermon-audio-file-container"><?php fcc_stow_sermon_the_audio_file_link() ?></div>
